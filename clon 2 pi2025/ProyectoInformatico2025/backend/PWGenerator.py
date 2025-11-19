fde14b86b6e03c1192cd652f7990ccb3707f5ef1
Nahuel Darian Tear <elnegro1220@gmail.com>
2025-11-19 01:45:31 -0300

    Delete clon 2 pi2025/ProyectoInformatico2025 directory

--- clon 2 pi2025/ProyectoInformatico2025/backend/PWGenerator.py
+++
@@ -1,7 +0,0 @@
-#Con esta rutina genero contraseñas encriptadas para los Usuarios
-from werkzeug.security import generate_password_hash
-
-print("#### Password Generator ####")
-print("============================")
-password = input("Ingrese una Contraseña: ")
-print("La contraseña generada es: ", generate_password_hash(password))
